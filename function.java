246a5b733307feb64fabdd138064a3b867d4f10a
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-16 00:59:10 +0530

    commit

--- function.java
+++ function.java
@@ -1,12 +1,45 @@
+// import java.util.*;
+// public class function{
+//     public static void printName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         String name= sc.nextLine();
+//         printName(name);// call our function
+//     }
+// }
+
+//add 2 num
+
+// import java.util.*;
+// public class function{
+//     public static int addNum(int a,int b){
+//         int sum = a+b;
+//         // System.out.println(sum);
+//         return sum;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a= sc.nextInt();
+//         int b= sc.nextInt();
+//         int sum = addNum(a,b);
+//         System.out.println(sum);
+//     }
+// }
+
+//multiply 2 num
 import java.util.*;
 public class function{
-    public static void printName(String name){
-        System.out.println(name);
-        return;
+    public static int product(int a,int b){
+        return a*b;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String name= sc.nextLine();
-        printName(name);// call our function
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        
+        System.out.println("product of 2 num:"+product(a,b));
     }
 }
