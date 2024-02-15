d1a5b8d9db607da720516791fbaadc8391bd159a
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-15 23:16:25 +0530

    Commit

---
+++ swtich.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class swtich{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        switch(button){
+            case 1: System.out.println("kashish");
+            break;
+            case 2: System.out.println("Priyanshi");
+            break;
+            case 3: System.out.println("pihu");
+            break;
+            case 4: System.out.println("default");
+            
+        }
+    }
+}
+//In Java, break is used to terminate the current loop or switch statement immediately
