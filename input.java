5c9e21ad6a2e38dbb5d860d5bf9d9a4fca7fd34f
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-15 21:54:07 +0530

    Commit

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       // String Name = sc.next();
+        //In Java, `next()` reads input until space, while `nextLine()` reads input until a line change (Enter key).
+        //nextInt()
+        //nextFloat()
+        String Name = sc.nextLine();
+        System.out.println(Name);
+    }
+}
