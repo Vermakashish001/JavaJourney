0f32a6dd92dcc56f09b4aafaffdb65fdfda8e707
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-16 00:07:16 +0530

    commit

---
+++ doWhileLoop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class doWhileLoop{
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<10);
+    }
+}
