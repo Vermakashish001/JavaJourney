1cbc7d726f2afe6181cd182a9475f7796d1c3114
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-16 00:24:50 +0530

    commit

--- forLoop.java
+++ forLoop.java
@@ -1,10 +1,38 @@
+// import java.util.*;
+// public class forLoop{
+//     public static void main(String[] args){
+//         for(int counter = 0; counter <10; counter = counter + 1){
+//             System.out.println(counter);
+//             System.out.println("kashish");
+//         }
+//     }
+// }
+//counter = counter + 1 => counter++
+
 import java.util.*;
 public class forLoop{
     public static void main(String[] args){
-        for(int counter = 0; counter <10; counter = counter + 1){
-            System.out.println(counter);
-            System.out.println("kashish");
-        }
+        // int sum = 0;
+        // for(int i =1; i <=4; i++){
+        //     sum = sum +i;
+        // }
+        // System.out.println(sum);
+
+            // Scanner sc = new Scanner(System.in);
+            // int n = sc.nextInt();
+            // int sum = 0;
+            // for(int i =1; i <=n; i++){
+            //     sum = sum + i;
+            // }
+            // System.out.println(sum);
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for(int i =1; i<11; i++){
+               
+                System.out.println(n*i);
+            }
+            
+
     }
 }
-//counter = counter + 1 => counter++
