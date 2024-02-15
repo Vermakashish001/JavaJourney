4969f2b2b3db13654fb1f4b19f2407007e965e42
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-15 21:00:06 +0530

    Initial commit

---
+++ First.java
@@ -0,0 +1,14 @@
+public class First{
+    public static void main(String[] args){
+        System.out.println("kashish");
+        int fistNum = 1233;
+        int secondNum = 7585;
+        // + - * / % ++ --
+        int sum = fistNum + secondNum;
+        double div = (double)fistNum / (double)secondNum;
+        System.out.println(div);
+        System.out.println(sum);
+
+    }
+}
+  
