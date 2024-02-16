08d8969db1bb94c6a7327c20cd61a59876326f18
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-16 21:44:16 +0530

    Commit

--- function.java
+++ function.java
@@ -30,16 +30,42 @@
 // }
 
 //multiply 2 num
+// import java.util.*;
+// public class function{
+//     public static int product(int a,int b){
+//         return a*b;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a= sc.nextInt();
+//         int b= sc.nextInt();
+        
+//         System.out.println("product of 2 num:"+product(a,b));
+//     }
+// }
+
+
+//factoriyal
 import java.util.*;
 public class function{
-    public static int product(int a,int b){
-        return a*b;
+    
+    public static void printFactorial(int n){
+        if(n<0){
+            System.out.println("invalid");
+            return;
+        }
+        int factorial=1;
+        for(int i = n ; i>=1 ;i--){
+            factorial = factorial*i;
+        }
+        System.out.println(factorial);
+        return;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a= sc.nextInt();
-        int b= sc.nextInt();
+        int n= sc.nextInt();
         
-        System.out.println("product of 2 num:"+product(a,b));
+        
+        printFactorial(n);
     }
 }
