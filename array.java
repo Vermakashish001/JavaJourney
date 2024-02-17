443aaefcf97a314d0a26edaefaa2f854677697f0
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-17 17:08:26 +0530

    commit

---
+++ array.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+// public class array{
+//     public static void main(String[] args){
+//         // int[] marks = new int[3];
+//         // marks[0]=98;
+//         // marks[1]=88;
+//         // marks[2]=86;
+//         int marks[] ={98,88,86};
+//         // System.out.println(marks[0]);
+//         for(int i=0; i<3; i++){
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
+import java.util.*;
+public class array{
+    public static void main(String[] args){
+        
+    }
+}
