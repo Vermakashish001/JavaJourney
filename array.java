13210405e425c17a64794f01c24245731f3034ab
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-20 23:13:05 +0530

    Commit

--- array.java
+++ array.java
@@ -14,6 +14,22 @@
 // }
 
 //Create an array by taking input from the user.
+// import java.util.*;
+// public class array{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int number[]= new int [size];
+//         for(int i = 0 ; i<size;i++){
+//             number[i] = sc.nextInt();
+//         }
+//         for(int i =0; i <size; i++){
+//             System.out.println(number[i]);
+//         } 
+//     }
+// }
+
+//take an array as input from the user Search for a given number x and print the index at which it occurs
 import java.util.*;
 public class array{
     public static void main(String[] args){
@@ -23,8 +39,14 @@ public class array{
         for(int i = 0 ; i<size;i++){
             number[i] = sc.nextInt();
         }
+
+        int x = sc.nextInt();
         for(int i =0; i <size; i++){
-            System.out.println(number[i]);
+            if(number[i]==x){
+                System.out.println("x found at index:"+i);
+            }
         } 
     }
 }
+
+//linear Search
