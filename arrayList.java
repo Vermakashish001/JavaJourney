b136caf1f76767557652311498afb08a4583cccd
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-22 14:01:53 +0530

    commit

--- arrayList.java
+++ arrayList.java
@@ -1,42 +1,56 @@
+// import java.util.*;
+// public class arrayList{
+//     public static void main(String []args){
+//         ArrayList <Integer> list = new ArrayList <>();
+//         //add element
+//         list.add(0);
+//         list.add(2);
+//         list.add(3);
+//         System.out .println(list);
+
+//         //get element
+//         int element = list.get(0);
+//         System.out.println(element);
+
+//         //add element in between
+//         list.add(1,1);
+//         System.out.println(list);
+
+//         //set element
+//         list.set(0,1);
+//         System.out.println(list);
+
+//         // delete element
+//         list.remove(3);
+//         System.out.println(list);
+
+//         //size
+//         int size = list.size();
+//         System.out.println(list);
+
+//         //loop
+//         for(int i = 0 ; i <list.size();i++){
+//             System.out.print(list.get(i));
+//         }
+//         System.out.println();
+
+//         //sorting
+//         Collections.sort(list);
+//         System.out.println(list);
+
+//     }
+// }
+
 import java.util.*;
-public class arrayList{
-    public static void main(String []args){
-        ArrayList <Integer> list = new ArrayList <>();
-        //add element
-        list.add(0);
-        list.add(2);
-        list.add(3);
-        System.out .println(list);
-
-        //get element
-        int element = list.get(0);
-        System.out.println(element);
-
-        //add element in between
-        list.add(1,1);
-        System.out.println(list);
-
-        //set element
-        list.set(0,1);
-        System.out.println(list);
-
-        // delete element
-        list.remove(3);
-        System.out.println(list);
-
-        //size
-        int size = list.size();
-        System.out.println(list);
-
-        //loop
-        for(int i = 0 ; i <list.size();i++){
-            System.out.print(list.get(i));
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int value = 1;
+        while(value <= 64) { 
+            list.add(value);
+            value *= 2;
         }
-        System.out.println();
-
-        //sorting
-        Collections.sort(list);
         System.out.println(list);
-
     }
 }
+
