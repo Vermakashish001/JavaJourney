13d6aad3dfdfe49585d66bec5283e9732e6b0e06
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-25 23:43:05 +0530

    commit

--- stringDeclaration.java
+++ stringDeclaration.java
@@ -19,5 +19,26 @@ public class stringDeclaration{
         for(int i =0;i<fullName.length(); i++){
             System.out.println(fullName.charAt(i));
         }
+
+        //compare
+        String name1 = "kashish";
+        String name2 = "zebra";
+        // 1 S1 > S2 : +ve valiue
+        // 2 S1 == S2 :0
+        // 3 S1 <s2: -ve value
+
+        //hello wello 
+        if(name1.compareTo(name2)==0){
+            System.out.println("String are equal");
+        }else {
+            System.out.println("String are not equal");
+        }
+
+        //compare
+        // String sentance = " My name is Kashish";
+        String sentance = "KashishVerma";
+        String name = sentance.substring(0,7);
+        System.out.println(name);
     }
 }
+// strings are immutable
