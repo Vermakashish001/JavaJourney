dfed330482217e7749350b131b0ba19f508513ff
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-25 19:36:50 +0530

    Commit

---
+++ stringDeclaration.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class stringDeclaration{
+    public static void main(String[] args){
+        // String name ="kashish";
+        // String fullName = "kashish Verma";
+        // String sentance= "i am in pain";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("your name is :")
+
+    }
+}
